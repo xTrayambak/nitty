57bccfaf312e2ddf7c88f6a49d7cc4107f574ae8
xTrayambak <xtrayambak@gmail.com>
2026-04-22 18:23:30 +0530

    add: nix flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A high-performance, GPU-accelerated terminal emulator written in Nim";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit stdenv.hostPlatform.system };
+      in
+      {
+        packages.default = pkgs.nimPackages.buildNimPackage {
+          pname = "nitty";
+	  version = "0.1.0";
+	  src = ./.;
+
+	  buildInputs = with pkgs; [
+            libvterm-neovim
+            wayland
+            libxkbcommon
+	    fontconfig
+            libGL
+          ];
+
+	  nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+	  nimFlags = ["--define:release", "--opt:speed", "--define:lto"];
+
+	  meta = with pkgs.lib; {
+            description = "A high-performance, GPU-accelerated terminal emulator written in Nim";
+	    platforms = platforms.linux;
+	    license = licenses.bsd3;
+	  };
+	};
+
+	devShells.default = pkgs.mkShell {
+          buildInputs = self.packages.${system}.default.buildInputs ++ (with pkgs; [
+            nim
+            nimble
+          ]);
+        };
+      }
+    )
+}
